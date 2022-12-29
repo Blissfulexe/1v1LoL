a27af90f4c0acc4af72b03cc16ea3007a0f91280
iraidyou <108106253+Blissfulexe@users.noreply.github.com>
2022-12-28 22:02:54 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const button = document.querySelector("#btn")
+function myWindow1() {
+ let myWindow1 = window.open('', 'myWindow1', 'scrollbars=1,height='+screen.availHeight+',width='+screen.availWidth);
+ myWindow1.document.write('<!DOCTYPE html>\n\
+<title>Game</title>\n\
+<p><iframe src="https://docs-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/ko1ov/to@1e8a949f89fcf2b110640c41a0705db01405d161/d7uf4.xml" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%">\n\
+<script>\n\
+alert("");\n\
+<\x2fscript>');
+}
+button.onclick = () => {
+alert("If this Window is blocked, you are screwed, LoL.")
+myWindow1();
+}
